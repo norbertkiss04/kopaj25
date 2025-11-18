d39587985d535f6107907beb20a9eda47f6ed349
OcztosKaroly <126339777+OcztosKaroly@users.noreply.github.com>
2025-11-18 20:24:50 +0100

    Update task1.py

--- server/tasks/karcsi/task1.py
+++ server/tasks/karcsi/task1.py
@@ -1,7 +1,15 @@
+from typing import Dict, Any, List
+
 from fastapi import APIRouter
+from pydantic import BaseModel
 
-router = APIRouter(prefix="")
 
-@router.get("/task1")
-def task():
-    return {"msg": "Gest request"}
+router = APIRouter( prefix="")
+
+# @router.get("/ground/task3")
+# def ground3(request):
+#     print(request.query_params)
+
+@router.post("/ground/task1")
+def return_status_ok(request):
+    return 200
